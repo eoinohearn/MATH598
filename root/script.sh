6966f46453380e4747efe5ecbb6bf2024255470d
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-07 13:27:23 -0500

    bash script

---
+++ root/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=Games_on_Graphs
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=edohearn@crimson.ua.edu
+#SBATCH -n 1 #tasks
+#SBATCH -N 1 #nodes
+#SBATCH -c 1 #number of cores per task
+
+#SBATCH -o Output/output_GoG.txt
+#SBATCH -p main
+#SBATCH --qos main
+#SBATCH --mem=20G
+
+
+
+cd /home/edohearn/MATH598/
+.venv/Scripts/activate
+python /home/edohearn/MATH598/root/main.py
