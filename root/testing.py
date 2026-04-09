fc5d4c1ec6a829b923843e2ce421febe871cd6a3
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-08 22:25:44 -0500

    array works!

--- root/testing.py
+++ root/testing.py
@@ -10,7 +10,7 @@ N = 100
 # p_values = [2/N, 4/N, 6/N, 8/N, 10/N]
 P = 2/N
 benefit = float(sys.argv[1])
-print(benefit)
+
 # RATIO = (5,1)
 # ratios = [(16,10),(17,10),(18,10),(19,10),(20,10),(21,10),(22,10),(23,10),(24,10)]
           
@@ -18,7 +18,7 @@ print(benefit)
 print("p, k, b/c, n, result")
 
 
-for _ in range(10):
+for _ in range(1000):
     G = EGraph(N,P, benefit, 1)
     n=0
     while(True):
