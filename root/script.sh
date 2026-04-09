97b567721ab8febddbadbee5fa56caa0c1e8b857
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-08 21:17:49 -0500

    notes and v high b/c

--- root/script.sh
+++ root/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=Games_on_Graphs5
+#SBATCH --job-name=Games_on_Graphs6
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edohearn@crimson.ua.edu
 #SBATCH -n 1 #tasks
 #SBATCH -N 1 #nodes
 #SBATCH -c 1 #number of cores per task
 
-#SBATCH -o Output/output_GoG5.txt
+#SBATCH -o Output/output_GoG6.txt
 #SBATCH -p main
 #SBATCH --qos main
 #SBATCH --mem=20G
