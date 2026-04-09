06cc393575e895d155ddc7bab9fdcdaf727b3ec1
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-09 15:14:05 -0500

    prod run k=10

--- root/script.sh
+++ root/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Games_on_Graphs_8
+#SBATCH --job-name=Games_on_Graphs_10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edohearn@crimson.ua.edu
 #SBATCH -n 1 #tasks
@@ -7,12 +7,12 @@
 #SBATCH -c 1 #number of cores per task
 #SBATCH --array 0-8
 
-#SBATCH -o Output/output_GoG_8/%a.txt
+#SBATCH -o Output/output_GoG_10/%a.txt
 #SBATCH -p main
 #SBATCH --qos main
 #SBATCH --mem=20G
 
-benefit=(6.0 6.5 7.0 7.5 8.0 8.5 9.0 9.5 10)
+benefit=(8.0 8.5 9.0 9.5 10 10.5 11 11.5 12)
 
 module load python/python3/3.11.7
 cd /home/edohearn/MATH598/
