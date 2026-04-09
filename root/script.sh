ac2d6e8f6987fb6b097fd54829dfac2b05aa7a9c
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-09 10:54:15 -0500

    larger step size test

--- root/script.sh
+++ root/script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=Games_on_Graphs_4
+#SBATCH --job-name=Games_on_Graphs_4a
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edohearn@crimson.ua.edu
 #SBATCH -n 1 #tasks
 #SBATCH -N 1 #nodes
 #SBATCH -c 1 #number of cores per task
-#SBATCH --array 0-9
+#SBATCH --array 0-8
 
-#SBATCH -o Output/output_GoG_4_%a.txt
+#SBATCH -o Output/output_GoG_4a/%a.txt
 #SBATCH -p main
 #SBATCH --qos main
 #SBATCH --mem=20G
 
-benefit=(3.6 3.7 3.8 3.9 4 4.1 4.2 4.3 4.4)
+benefit=(2.0 2.5 3.0 3.5 4 4.5 5.0 5.5 6.0)
 
 module load python/python3/3.11.7
 cd /home/edohearn/MATH598/
