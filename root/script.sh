3c244087756c2d798ef79eaa98523e98c0d5a74e
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-08 20:42:57 -0500

    almost messed up!

--- root/script.sh
+++ root/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=Games_on_Graphs4
+#SBATCH --job-name=Games_on_Graphs5
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edohearn@crimson.ua.edu
 #SBATCH -n 1 #tasks
 #SBATCH -N 1 #nodes
 #SBATCH -c 1 #number of cores per task
 
-#SBATCH -o Output/output_GoG4.txt
+#SBATCH -o Output/output_GoG5.txt
 #SBATCH -p main
 #SBATCH --qos main
 #SBATCH --mem=20G
