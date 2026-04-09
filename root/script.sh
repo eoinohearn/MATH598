ba8118bb812d8019a050567cdd2556b1ad700b1e
Eoin O'Hearn <eoin.ohearn@gmail.com>
2026-04-08 22:17:14 -0500

    more writing plus conversion to array job

--- root/script.sh
+++ root/script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=Games_on_Graphs6
+#SBATCH --job-name=Games_on_Graphs_ArrayTry
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edohearn@crimson.ua.edu
 #SBATCH -n 1 #tasks
 #SBATCH -N 1 #nodes
 #SBATCH -c 1 #number of cores per task
+#SBATCH --array 0-8
 
-#SBATCH -o Output/output_GoG6.txt
+#SBATCH -o Output/output_GoG_4_%a.txt
 #SBATCH -p main
 #SBATCH --qos main
 #SBATCH --mem=20G
 
+benefit=(3.6 3.7 3.8 3.9 4 4.1 4.2 4.3 4.4)
 
 module load python/python3/3.11.7
 cd /home/edohearn/MATH598/
-python /home/edohearn/MATH598/root/testing.py
+python /home/edohearn/MATH598/root/testing.py ${benefit[${SLURM_ARRAY_TASK_ID}]}
--- root/testing.py
+++ root/testing.py
@@ -1,37 +1,41 @@
 from functions import *
 from EGraph import EGraph
+import sys
 
 
 N = 100
-EPOCH = 10000
+# EPOCH = 10000
 # k is about equal to Np+2
 
 # p_values = [2/N, 4/N, 6/N, 8/N, 10/N]
 P = 2/N
-RATIO = (5,1)
+benefit = sys.argv[1]
+# RATIO = (5,1)
 # ratios = [(16,10),(17,10),(18,10),(19,10),(20,10),(21,10),(22,10),(23,10),(24,10)]
           
 
 print("p, k, b/c, n, result")
 
 
-for _ in range(10000):
-    G = EGraph(N,P, RATIO[0], RATIO[1])
-    length=EPOCH
-    for n in range(EPOCH):
+for _ in range(10):
+    G = EGraph(N,P, benefit, 1)
+    n=0
+    while(True):
+        n+=1
         G.calculate_fitness()
         G.update_rule()
         x,y = G.get_stats()
         if x == 0:
-            length=n
             break
             
         elif x==N:
-            length=n
+            break
+
+        if n>= 100000:
             break
 
 
-    print(f"{P},{G.k},{RATIO[0]/RATIO[1]}, {length}, {x}")
+    print(f"{P},{G.k},{benefit}, {n}, {x}")
                 
 
 
